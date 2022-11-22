13dca6aa167523340db7ba368427c4f5e117ff3c
Janusz Dalecki <rejkid@optusnet.com.au>
2022-11-22 12:30:00 +1100

    r

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,
-  //apiUrl: 'https://rejkid.hopto.org:4000'
-  //apiUrl: 'http://rejkid.hopto.org:4000'
-  apiUrl: 'https://rosterbackend.azurewebsites.net'
+  //apiUrl: 'https://49.176.185.39:4000'
+  //apiUrl: 'https://rosterbackend.azurewebsites.net'
   //apiUrl: 'http://localhost:4000'
+  //apiUrl: 'http://rejkid.hopto.org:4000'
+  apiUrl: 'https://rejkid.hopto.org:4000'
 };
