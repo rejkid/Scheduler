07d6f6b8a8ef1fae4865c7dbc1dd9a8e1880abc8
Janusz Dalecki <rejkid@optusnet.com.au>
2022-11-24 08:13:12 +1100

    Revert "Going back to version 4.5.0 with bootstrap"
    This reverts commit 56803bd8aded87e848d6fcb9f4622535ec25609a.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   //apiUrl: 'https://49.176.185.39:4000'
-  apiUrl: 'https://rosterbackend.azurewebsites.net'
+  //apiUrl: 'https://rosterbackend.azurewebsites.net'
   //apiUrl: 'http://localhost:4000'
-  //apiUrl: 'http://rejkid.hopto.org:4000'
-  //apiUrl: 'https://rejkid.hopto.org:4000'
+  ///apiUrl: 'http://rejkid.hopto.org:4000'
+  apiUrl: 'https://rejkid.hopto.org:4000'
 };
