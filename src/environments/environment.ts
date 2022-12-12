5f5c4509a318e652076b597cae36998ed0d4c8db
Janusz Dalecki <rejkid@optusnet.com.au>
2022-12-12 17:50:15 +1100

    Fixed env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,8 +7,8 @@ export const environment = {
   dateFormat: 'DD-MM-YYYY hh:mm',
 
   //apiUrl: 'https://49.176.185.39:4000'
-  //apiUrl: 'https://rosterbackend.azurewebsites.net'
-  apiUrl: 'http://localhost:4000'
+  apiUrl: 'https://rosterbackend.azurewebsites.net'
+  //apiUrl: 'http://localhost:4000'
   //apiUrl: 'http://rejkid.hopto.org:4000'
   //apiUrl: 'https://rejkid.hopto.org:4000'
 };
