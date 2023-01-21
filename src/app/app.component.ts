a8cbde0a4e50dbd7f4a9a2a24a11f712cf887949
Janusz Dalecki <rejkid@optusnet.com.au>
2023-01-21 19:14:20 +1100

    Added missing component update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,14 @@ import { Account, Role } from './_models';
 
 @Component({ selector: 'app', templateUrl: 'app.component.html' })
 export class AppComponent {
+    isLoaded: boolean = false;
     Role = Role;
     account: Account = null;
 
     constructor(private accountService: AccountService) {
         this.accountService.account.subscribe(x => {
             this.account = x
+            this.isLoaded = true;
         });
     }
 
