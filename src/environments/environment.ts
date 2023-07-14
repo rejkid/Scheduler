c5f589dfc1e6b9a675cdb21692496df345d65b42
rejkid <rejkid@gmail.com>
2023-07-15 09:05:36 +1000

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,12 +9,13 @@ export const environment = {
   
 
   //apiUrl: 'https://49.176.185.39:4000'
-  //apiUrl: 'https://rosterbackend.azurewebsites.net'
+  apiUrl: 'olor.azurewebsites.net',
   //apiUrl: 'http://localhost:4000'
   //apiUrl: 'http://rejkid.hopto.org:4000'
-  apiUrl: 'https://rejkid.hopto.org:4000',
+  //apiUrl: 'https://rejkid.hopto.org:4000',
 
-  baseUrl: 'https://rejkid.hopto.org:4000'
+  //baseUrl: 'https://rejkid.hopto.org:4000',
+  baseUrl: 'olor.azurewebsites.net'
 };
 
 /*
